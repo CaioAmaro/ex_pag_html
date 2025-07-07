e5931e41e7660942707fa9c4344eca94a67e2a3d
Caio Amaro <54718957+CaioAmaro@users.noreply.github.com>
2025-07-07 09:02:05 -0300

    Add footer loading functionality to index page

---
+++ load-footer.js
@@ -0,0 +1,11 @@
+// load-header.js
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+    })
+    .catch(error => {
+      console.error("Erro ao carregar o header:", error);
+    });
+});
